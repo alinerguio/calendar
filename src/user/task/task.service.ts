0ac4679025ee82976647c76afc82cae29c62a0f1
alinerguio <alinerguio@gmail.com>
2021-04-12 10:31:08 -0300

    Task validando user

--- src/user/task/task.service.ts
+++ src/user/task/task.service.ts
@@ -1,5 +1,6 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
+import { User } from '../user.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { Task } from './task.model';
 
@@ -11,10 +12,26 @@ export class TaskService {
     ){}
 
     async createTask(createTask: CreateTaskDto) {
+        const user = await User.findOne({ 
+            where: { id: createTask.userId },
+        }); // SELECT * FROM User WHERE id = task.userId
+
+        if (!user) {
+            throw new BadRequestException('Não foi possível criar a task, usuário não existente');
+        }
+
         return this.taskModel.create(createTask);
     }
 
-    findTasks(userId: number): Promise<Task[]> {
+    async findTasks(userId: number): Promise<Task[]> {
+        const user = await User.findOne({ 
+            where: { id: userId },
+        }); // SELECT * FROM User WHERE id = task.userId
+
+        if (!user) {
+            throw new BadRequestException('Não foi possível listar as tasks, usuário não existente.');
+        }
+
         const where: any = {}
         where.userId = userId;
         return this.taskModel.findAll({ where });
